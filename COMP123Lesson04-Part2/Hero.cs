30417b1ce5716d81a11725a392d1af0c2aba805b
kerenmenguito <kcee_charmaine@hotmail.com>
2015-01-27 13:44:52 -0500

    COMP123-Lesson04-Part2

---
+++ COMP123Lesson04-Part2/Hero.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123Lesson04_Part2
+{
+    class Hero
+    {
+        //PRIVATE PROPERTIES
+        private bool bravery = true;
+        private int health;
+        private int smart;
+        private string gender;
+
+        //CONSTRUCTOR +++++++++++++++++++
+        public Hero(int health, int smart, string gender) 
+        {
+            this.health = health;
+            this.smart = smart;
+            this.gender = gender;
+        }
+
+        //PUBLIC METHODS
+        public void doesGoodThings()
+        {
+            Console.WriteLine("Doin' good things... YEAH!");
+        }
+    }
+}
---
+++ COMP123Lesson04-Part2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123Lesson04_Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero myHero = new Hero(7, 8,"female");
+            myHero.doesGoodThings();
+
+            //Wait for Input before closing console
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+        }
+    }
+}
